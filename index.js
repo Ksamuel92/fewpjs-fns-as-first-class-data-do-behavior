30512c529b7f9190dfa602fe4ffdfa91341c53c9
Kyle Samuel <ksamuel92@gmail.com>
2021-08-19 12:14:45 -0400

    commit tree 08e163fe3365ce9b0c60dc26139df624831e33fe

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+ let timeArray = timeString.split(":");
+ let hour = parseInt(timeArray[0])
+ if (hour < 12) {
+   console.log(hour)
+  return  'Good Morning'
+ } else if (12 < hour && hour < 17) {
+  console.log(hour)
+   return 'Good Afternoon'
+ } else if (hour > 17) {
+  console.log(hour)
+   return 'Good Evening'
+ }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+ let node = document.getElementById('greeting')
+ node.innerHTML = string
+}
